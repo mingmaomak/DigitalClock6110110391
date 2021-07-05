40816b8e264e8c73063c2af232800ed53e6dcce1
mingmaomak <61099491+mingmaomak@users.noreply.github.com>
2021-07-05 19:37:47 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,11 +2,10 @@ import 'dart:io';
 import 'dart:convert';
 import 'dart:async';
 
-void main() async {
+void main() async 
+{
   final file = File('input.txt');
-  var linelist = [];
-  var inputlist = [];
-  var modlist = [];
+  var linelist = []; //storing input as lines from input.txt
   var hour = -1; //initialize hour integer
   var min = -1; //initialize minute integer
 
@@ -14,51 +13,24 @@ void main() async {
       .openRead()
       .transform(utf8.decoder) // Decode bytes to UTF-8.
       .transform(LineSplitter()); // Convert stream to individual lines.
-  try {
-    await for (var line in lines) {
+  try 
+  {
+    await for (var line in lines) 
+    {
       linelist.add(line); //add each line as an item in "linelist" list
     }
-    //print('File is now closed.');
-  } catch (e) {
-    print('Error: $e');
-  }
-  for (var l in linelist) {
-    FindColon:
-    for (int i = 0; i < l.length; i++) {
-      if (l[i] == ':') {
-        modlist.add(l.substring(0, i));
-        inputlist.add(l.substring(i + 1, l.length));
-        break FindColon;
-      }
-    }
+  } 
+  catch (e) 
+  {
+    print('Error: $e'); //print exception if caught
   }
 
-  //print('line 35');
-  // print ('modlist'); for (var l in modlist) { print('$l');}
-  // print ('inputlist'); for (var l in inputlist) { print('$l');}
-  for (int i = 0; i < modlist.length; i++) {
-    //loop through all mods
-    //tempo = inputlist[i];
-    // print('I passed $i');
-    var mod =
-        modlist[i].split(','); //get list of each modification in a modlist item
-    for (var m in mod) {
-      //modify each item in inputlist
-      if (m[0] == 'U') {
-        inputlist[i] = inputlist[i].toUpperCase();
-      }
-      if (m[0] == 'L') {
-        var ts = m.substring(1);
-        var tt = int.parse(ts);
-        inputlist[i] = inputlist[i].substring(0, tt);
-      }
-      if (m[0] == 'S') {
-        inputlist[i] = inputlist[i].replaceAll(' ', '');
-      }
-    }
-  }
-
-  for (var l in inputlist) {
+  print ('linelist'); 
+  for (var l in linelist) //loop for each item in "linelist"
+  { 
     print('$l');
+
   }
+
+
 }
